432d5ffd70eda9937997e1a4d8b52b2baac1cc2f
Progressor <mohammadalfahd040@gmail.com>
2026-07-20 21:10:04 +0530

    Added HTTP client Using libcurl

---
+++ include/api_call.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <curl/curl.h>
+
+class network_client{
+    public:
+    
+    CURL* curl;
+    CURLcode result;
+
+    std::string response;
+
+    std::string symbol="BTC-USD";
+    std::string interval="1h";
+    size_t limit=200;
+    time_t start_time;
+    time_t end_time;
+
+    long timeout=5L;
+    long follow_redirects=1L;
+    std::string user_agent="TerminalView";
+    std::string accept_encoding="";
+    struct curl_slist* headers=nullptr;
+
+    //temp for debug only
+    long verbose=1L;
+    char errorbuffer[CURL_ERROR_SIZE]={0};
+
+    network_client(){
+        curl=curl_easy_init();
+
+        if(!curl){
+            std::cout<<"Failed Initialization\n";
+            return ;
+        }
+    }
+
+    ~network_client(){
+        if(curl){
+            curl_easy_cleanup(curl);
+        }
+        if(headers){
+            curl_slist_free_all(headers);
+        }
+    }
+
+    static size_t writecallback(void* content,size_t size,size_t nmemb,void* userp);
+    void setup(std::string &url);
+    bool fetch_data();
+};
+
---
+++ src/api_call.cpp
@@ -0,0 +1,52 @@
+#include "../include/api_call.h"
+
+size_t network_client :: writecallback(void* content ,size_t size,size_t nmemb,void* userp){
+
+    size_t totalbytes=size*nmemb;
+    std::string* response=static_cast<std::string*>(userp);
+    response->append(static_cast<char*>(content),totalbytes);
+
+    return totalbytes;
+}
+
+void network_client :: setup(std::string &url){
+
+    if(!curl){
+        std::cout<<"Failed Initialization\n";
+        return;
+    }
+    
+    headers=curl_slist_append(headers,"Accept: application/json");
+
+    curl_easy_setopt(curl,CURLOPT_URL,url.c_str());
+    curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION,writecallback);
+    curl_easy_setopt(curl,CURLOPT_WRITEDATA,&response);
+    curl_easy_setopt(curl,CURLOPT_FOLLOWLOCATION,follow_redirects);
+    curl_easy_setopt(curl,CURLOPT_ACCEPT_ENCODING,accept_encoding.c_str());
+    curl_easy_setopt(curl,CURLOPT_USERAGENT,user_agent.c_str());
+    curl_easy_setopt(curl,CURLOPT_TIMEOUT,timeout);
+    curl_easy_setopt(curl,CURLOPT_HTTPHEADER,headers);
+    curl_easy_setopt(curl,CURLOPT_VERBOSE,verbose);
+    curl_easy_setopt(curl,CURLOPT_ERRORBUFFER,errorbuffer);
+
+}
+
+bool network_client :: fetch_data(){
+    
+    response.clear();
+    
+    result =curl_easy_perform(curl);
+
+    if(result!=CURLE_OK){
+        std::cerr<<"Request Failed \n";
+        std::cerr<<"libcurl Error : "<<curl_easy_strerror(result)<<std::endl;
+        std::cerr<<"Detailed Error : "<<errorbuffer<<std::endl;
+
+        return false;
+    }
+    else{
+        
+        return true;
+    }
+    
+}
